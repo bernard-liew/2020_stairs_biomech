d3530c0e3e21211faa299f2ace49e4f05a0277eb
bernard-liew <liew_xwb@hotmail.com>
2020-07-10 22:32:55 +0100

    added bam with te terms

---
+++ code/bam_ankle_allspeed.R
@@ -0,0 +1,44 @@
+# Load packages ---------------------------------------------------------------------
+
+# Helper
+library (dplyr)
+
+# modelling
+library (mgcv)
+library (mgcViz)
+
+
+# Import data ------------------------------------------------------------------------
+
+frac <- 1
+
+dat <- readRDS("output/df_clean_allspeed.RDS")  %>%
+  filter (joint == "ankle") %>% 
+  filter(study != "lencioni") %>% # data reported dissimilar to others
+  group_by(subj, speed, age, sex, ht, wt, study)%>%
+  sample_frac(frac) %>%
+  ungroup () %>%
+  mutate(sex = as.factor(sex),
+         subj = as.factor(subj),
+         study = as.factor(study)) %>%
+  as.data.frame()
+
+
+
+form <-  val ~ te(cycle, speed, age, k = c(40, 5, 5), bs = c("cr",  "cr",  "cr")) +
+  ti(ht, bs = "cr") +
+  s(subj, bs = 're') +
+  sex
+
+# Modelling ------------------------------------------------------------------------
+
+
+mod <- bam (form,
+            data = dat,
+            discrete = TRUE,
+            family = scat())
+
+b <- getViz(mod)
+check (b)
+
+saveRDS(mod, "output/bam_allspeed_ankle.RDS")
