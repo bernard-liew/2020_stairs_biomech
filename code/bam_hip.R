947b0d313d8a8aff70ab4eba2c6d85491baf5913
bernard-liew <liew_xwb@hotmail.com>
2020-07-16 22:19:43 +0100

    added bam analysis for self speed

---
+++ code/bam_hip.R
@@ -0,0 +1,68 @@
+# Load packages ---------------------------------------------------------------------
+
+# Helper
+library (dplyr)
+
+# modelling
+library (mgcv)
+
+# plots
+library (mgcViz)
+library(itsadug)
+
+# Import data ------------------------------------------------------------------------
+
+frac <- 1
+
+dat <- readRDS("output/df_clean_self_outRm.RDS")  %>%
+  filter (joint == "hip") %>% 
+  filter(study != "lencioni") %>% # data reported dissimilar to others
+  group_by(subj, speed, age, sex, ht, wt, study)%>%
+  sample_frac(frac) %>%
+  ungroup () %>%
+  mutate(sex = as.factor(sex),
+         subj = as.factor(subj),
+         study = as.factor(study)) %>%
+  as.data.frame()
+
+
+
+bs <-  "cr"
+
+form <-  val ~  ti (cycle, k = 30, bs = bs) +
+  ti (age, k = 5, bs = bs) +
+  ti (speed, k = 5, bs = bs) +
+  ti (cycle, age, k = c(30, 5), bs = bs) +
+  ti (cycle, speed, k = c(30, 5), bs = bs) +
+  ti (age, speed, k = c(5, 5), bs = bs) +
+  ti (cycle, speed, age, k = c(15, 5, 5), bs = bs) + # adds to the computation time
+  ti (cycle, ht, k = c(15, 5), bs = bs) +
+  ti(ht, k = 5, bs = bs) +
+  ti (cycle, k = 15, by = study, bs = "re") + 
+  s(subj, bs = 're') +
+  sex
+
+# Modelling ------------------------------------------------------------------------
+
+
+mod <- bam (form,
+            data = dat,
+            discrete = TRUE,
+            family = scat())
+
+# Diagnostics ----------------------------------------------------------------------
+
+b <- getViz(mod)
+check (b)
+
+fit <- fitted(mod)
+res <- resid (mod)
+
+plot (fit, res)
+
+# Plot inference -------------------------------------------------------------------
+
+plot_smooth(mod, view = "cycle", cond = list (age = c(30), speed = 1), n.grid = 101, rm.ranef = TRUE)
+
+saveRDS(mod, "output/bam_selfspeed_hip_outRm.RDS")
+
