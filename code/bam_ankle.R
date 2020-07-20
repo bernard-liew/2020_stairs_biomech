9c0a142d9fe4498035f3f71e0bedd2ee8212ab6b
bernard-liew <liew_xwb@hotmail.com>
2020-07-20 07:00:26 +0100

    round speed and modified k basis

--- code/bam_ankle.R
+++ code/bam_ankle.R
@@ -12,13 +12,16 @@ library(itsadug)
 
 # Import data ------------------------------------------------------------------------
 
-frac <- 1
-
 dat <- readRDS("output/df_clean_self_outRm.RDS")  %>%
   filter (joint == "ankle") %>% 
   filter(study != "lencioni") %>% # data reported dissimilar to others
-  group_by(subj, speed, age, sex, ht, wt, study)%>%
-  sample_frac(frac) %>%
+  mutate (speed_rd  = round (speed, 1)) %>%
+  group_by(subj, study, joint, cond, speed_rd, cycle, sex) %>%
+  summarize (val = mean (val),
+             speed = round(mean (speed),1),
+             age = age[1],
+             ht = round (ht[1],2),
+             wt = wt[1]) %>%
   ungroup () %>%
   mutate(sex = as.factor(sex),
          subj = as.factor(subj),
@@ -28,28 +31,33 @@ dat <- readRDS("output/df_clean_self_outRm.RDS")  %>%
 
 
 bs <-  "cr"
+n_cyc <- length (unique(dat$cycle))
+n_age <- length (unique(dat$age))
+n_speed <- length (unique(dat$speed))
+n_ht <- length (unique(dat$ht))
 
-form <-  val ~  ti (cycle, k = 30, bs = bs) +
-  ti (age, k = 5, bs = bs) +
+form <-  val ~  ti (cycle, k = 20, bs = bs) +
+  ti (age, k = 25, bs = bs) +
   ti (speed, k = 5, bs = bs) +
-  ti (cycle, age, k = c(30, 5), bs = bs) +
-  ti (cycle, speed, k = c(30, 5), bs = bs) +
-  ti (age, speed, k = c(5, 5), bs = bs) +
-  ti (cycle, speed, age, k = c(15, 5, 5), bs = bs) + # adds to the computation time
-  ti (cycle, ht, k = c(15, 5), bs = bs) +
-  ti(ht, k = 5, bs = bs) +
-  ti (cycle, k = 15, by = study, bs = "re") + 
+  ti (cycle, age, k = c(20, 12), bs = bs) +
+  ti (cycle, speed, k = c(20, 5),  bs = bs) +
+  ti (age, speed,  k = c(12, 5),  bs = bs) +
+  ti (cycle, speed, age,  k = c(20, 5, 5), bs = bs) + # adds to the computation time
+  ti (cycle, ht, k = c(20, 10), bs = bs) +
+  ti(ht, k = 10, bs = bs) +
+  ti (cycle, k = 20, by = study, bs = "re") + 
   s(subj, bs = 're') +
   sex
 
 # Modelling ------------------------------------------------------------------------
 
-
-mod <- bam (form,
-            data = dat,
-            discrete = TRUE,
-            family = scat())
-
+system.time(
+  
+  mod <- bam (form,
+              data = dat,
+              discrete = TRUE,
+              family = scat())
+)
 # Diagnostics ----------------------------------------------------------------------
 
 b <- getViz(mod)
