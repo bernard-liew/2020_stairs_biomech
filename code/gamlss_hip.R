92dc9944d954a25a88aeb7ae99b9fe07730aabf6
bernard-liew <liew_xwb@hotmail.com>
2020-07-27 15:57:03 +0100

    modified k values and snip data for ankle

--- code/gamlss_hip.R
+++ code/gamlss_hip.R
@@ -18,33 +18,33 @@ frac <- 1
 dat <- readRDS("output/df_clean_self_outRm.RDS")  %>%
   filter (joint == "hip") %>% 
   filter(study != "lencioni") %>% # data reported dissimilar to others
-  mutate (speed_rd  = round (speed, 1)) %>%
-  group_by(subj, study, joint, cond, speed_rd, cycle, sex) %>%
-  summarize (val = mean (val),
-             speed = round(mean (speed),1),
-             age = age[1],
-             ht = ht[1],
-             wt = wt[1]) %>%
-  ungroup () %>%
-  mutate(sex = as.factor(sex),
-         subj = as.factor(subj),
-         study = as.factor(study)) %>%
+  # mutate (speed_rd  = round (speed, 1)) %>%
+  # group_by(subj, study, joint, cond, speed_rd, cycle, sex) %>%
+  # summarize (val = mean (val),
+  #            speed = round(mean (speed),1),
+  #            age = age[1],
+  #            ht = ht[1],
+  #            wt = wt[1]) %>%
+  # ungroup () %>%
+  mutate(sex = factor(sex),
+         subj = factor(subj),
+         study = factor(study)) %>%
   as.data.frame()
 
 bs <-  "cr"
 
-form <-  val ~ ba (~ ti (cycle, k = 20, bs = bs) +
-                     ti (age, k = 25, bs = bs) +
-                     ti (speed, k = 5, bs = bs) +
-                     ti (cycle, age, k = c(20, 12), bs = bs) +
-                     ti (cycle, speed, k = c(20, 5),  bs = bs) +
-                     ti (age, speed,  k = c(12, 5),  bs = bs) +
-                     ti (cycle, speed, age,  k = c(20, 5, 5), bs = bs) + # adds to the computation time
-                     ti (cycle, ht, k = c(20, 10), bs = bs) +
-                     ti(ht, k = 10, bs = bs) +
-                     ti (cycle, k = 20, by = study, bs = "re") + 
-                     s(subj, bs = 're') +
-                     sex)
+form <-  val ~ ba(~ti(cycle, k = 30, bs = bs) + 
+                    ti(age, k = 25, bs = bs) + 
+                    ti(speed, k = 5, bs = bs) + 
+                    ti(cycle, age, k = c(20, 12), bs = bs) + 
+                    ti(cycle, speed, k = c(20, 5), bs = bs) + 
+                    ti(age, speed, k = c(12, 5), bs = bs) + 
+                    ti(cycle, speed, age, k = c(20, 5, 5), bs = bs) + 
+                    ti(cycle, ht, k = c(20, 10), bs = bs) + 
+                    ti(ht, k = 10, bs = bs) + 
+                    ti(cycle, k = 20, by = study, bs = "re") + 
+                    s(subj, bs = "re") + 
+                    sex) 
 
 # Modelling ------------------------------------------------------------------------
 mod <- gamlss(form,
