5e35e4d2173fccd4ea2501253c32f5ce519d2737
Liew <bl19622@essex.ac.uk>
2020-10-23 16:08:25 +0100

    changed final models

--- code/final_models.R
+++ code/final_models.R
@@ -13,13 +13,13 @@ library(gamlss.add)
 source("code/settings_to_formula.R")
 # function to create final model on hold-out set
 # per joint
-create_final_model <- function(joint = "ankle" , 
+create_final_model <- function(joint = "ankle" ,
                                use_best_family = FALSE,
                                defined_family = "NO",
-                               data = NULL, 
+                               data = NULL,
                                withRE = TRUE)
 {
-  
+
   ############# read hold-out set ##############
   if(is.null(data)){
     data <- readRDS(paste0("output/", joint, "_splitted.RDS"))
@@ -28,54 +28,54 @@ create_final_model <- function(joint = "ankle" ,
     this_data <<- data
   }
   ##############################################
-  
+
   ############# get best mu form ###############
   bo <- readRDS(paste0("output/BO_",joint,".RDS"))
-  df_bo <- bo$scoreSummary %>% 
+  df_bo <- bo$scoreSummary %>%
     # drop the ones that did not work
-    filter(Score > -100) %>% 
+    filter(Score > -100) %>%
     mutate(Score = -Score)
-  
-  best_setting <- df_bo %>% 
-    arrange(Score) %>% 
-    dplyr::select(k_cycle:Score) %>%  
-    dplyr::select(k_cycle:k_cycle_re) %>% 
+
+  best_setting <- df_bo %>%
+    arrange(Score) %>%
+    dplyr::select(k_cycle:Score) %>%
+    dplyr::select(k_cycle:k_cycle_re) %>%
     head(1) %>% c()
   form <- do.call(settings_to_formula, best_setting)
-  if(withRE=="subj"){ 
-    form <- paste0("val ~ ba(", form, " + s(subj, bs = 're'))") 
-  }else if(withRE=="study") 
+  if(withRE=="subj"){
+    form <- paste0("val ~ ba(", form, " + s(subj, bs = 're'))")
+  }else if(withRE=="study")
   {
-    form <- paste0("val ~ ba(", form, " + s(study, bs = 're'))") 
+    form <- paste0("val ~ ba(", form, " + s(study, bs = 're'))")
   }else{
     form <- paste0("val ~ ba(", form, ")")
   }
   ##############################################
-  
+
   ############# get best family ################
   if(use_best_family)
   {
-    
+
     fams <- c(
-      "NO", "GU", "RG" ,"LO", "NET", "TF", "TF2", "PE","PE2", 
-      "SN1", "SN2", "exGAUS", "SHASH", "SHASHo","SHASHo2", 
-      "EGB2", "JSU", "JSUo", "SEP1", "SEP2", "SEP3", "SEP4", 
+      "NO", "GU", "RG" ,"LO", "NET", "TF", "TF2", "PE","PE2",
+      "SN1", "SN2", "exGAUS", "SHASH", "SHASHo","SHASHo2",
+      "EGB2", "JSU", "JSUo", "SEP1", "SEP2", "SEP3", "SEP4",
       "ST1", "ST2", "ST3", "ST4", "ST5", "SST", "GT"
     )
-    
+
     bestFam <- fams[which.min(unlist(readRDS(paste0("output/dist_comparison_",joint,".RDS"))))]
-    
+
   }else{
-    
+
     bestFam <- defined_family
-    
+
   }
   ##############################################
-  
-  
+
+
   #### define sig form and fit model ###########
   sig_formula <- "~ ba (~ ti(cycle, bs = 'cr', k = 20))"
-  
+
   mod <- gamlss(formula = as.formula(form),
                 family = bestFam,
                 sigma.fo = as.formula(sig_formula),
@@ -84,24 +84,24 @@ create_final_model <- function(joint = "ankle" ,
                 n.cyc = 200,
                 trace = TRUE)
   ##############################################
-  
+
   return(list(mod,form))
-  
+
 }
 
 # load original data
-dat <- readRDS("output/df_clean_allspeed.RDS") %>%
-  filter (cond %in% c("walkt05", "self", "c4")) %>% 
-  group_by(subj, speed, age, sex, ht, wt, study)%>% 
+dat <- readRDS("output/df_clean_self.RDS") %>%
+  filter (study != "lencioni") %>%
+  group_by(subj, speed, age, sex, ht, wt, study)%>%
   ungroup () %>%
-  mutate(sex = as.factor(sex),
-         subj = as.factor(subj),
-         study = as.factor(study)) %>%
+  mutate(sex = factor(sex),
+         subj = factor(subj),
+         study = factor(study)) %>%
   as.data.frame() %>%
-  arrange (study, subj, joint, cond, speed, cycle)  
+  arrange (study, subj, joint, cond, speed, cycle)
 
 # ankle
-mod_ankle <- create_final_model(joint = "ankle", data = dat %>% filter(joint=="ankle") %>% 
+mod_ankle <- create_final_model(joint = "ankle", data = dat %>% filter(joint=="ankle") %>%
   filter(cycle < 70 & cycle > 20))
 pdf("output/plots_ankle.pdf")
 # old.par <- par()
